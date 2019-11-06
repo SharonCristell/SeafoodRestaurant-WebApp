d0d794ac434599d615b0c860e2df8b54a1574122
Ingrid Arellano <email@gmail.com>
2019-11-06 13:06:41 -0500

    Crear mvc 2.0

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,15 +15,5 @@ namespace ProyectoPM.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
