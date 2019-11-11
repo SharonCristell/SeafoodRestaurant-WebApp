537ed0002c28d1da3b120a21321f54f9b0eab6cd
Sharon <sharonc.quispec@gmail.com>
2019-11-10 21:17:22 -0500

    Historial

---
+++ Models/Reserva.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoPM.Models
+{
+    public class Reserva
+    {
+        
+        [Required]
+        [Display(Name="Nombre")]
+        public string Nombre{get;set;}
+
+
+
+
+
+    }
+}
