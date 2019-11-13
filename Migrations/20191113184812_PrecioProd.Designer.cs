cede6827f25458a523b36321e5ca7a9cbbe6fd1b
Ingrid <ingrid10arellano@gmail.com>
2019-11-13 13:57:48 -0500

    Agrego Precio

--- Migrations/20191113184812_PrecioProd.Designer.cs
+++ Migrations/20191113184812_PrecioProd.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProyectoPM.Models;
 
 namespace ProyectoPM.Migrations
 {
     [DbContext(typeof(RestauranteContext))]
-    partial class RestauranteContextModelSnapshot : ModelSnapshot
+    [Migration("20191113184812_PrecioProd")]
+    partial class PrecioProd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20191113184812_PrecioProd.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProyectoPM.Migrations
+{
+    public partial class PrecioProd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Precio",
+                table: "Productos",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Precio",
+                table: "Productos");
+        }
+    }
+}
