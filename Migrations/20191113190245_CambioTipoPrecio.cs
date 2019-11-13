15be4706cf5951f23007cc7677439fd3d7558d32
Ingrid <ingrid10arellano@gmail.com>
2019-11-13 14:05:34 -0500

    Cambio Tipo Dato Precio

---
+++ Migrations/20191113190245_CambioTipoPrecio.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProyectoPM.Migrations
+{
+    public partial class CambioTipoPrecio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Precio",
+                table: "Productos",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Precio",
+                table: "Productos",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}
